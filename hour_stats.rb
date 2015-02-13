967fce67e68840c9baff3cc3d49600e21a1b46e2
Arup Sen <arupsen48@gmail.com>
2015-02-13 14:42:31 +0000

    fields for each activity, methods to sum and print totals for the year

--- hour_stats.rb
+++ hour_stats.rb
@@ -1,16 +1,37 @@
 # This simple script is to process a text file with space separated values.
 # Each line has a list of hour counts represented as floats with 15 minute increments.
 # Possible values 0 6.0 4.25 2.75 1.5
-# Six values - Cycling, Entertainment, Programming, Reading, Yoga
+# Five values - Cycling, Entertainment, Programming, Reading, Yoga
 # Object - show totals as follows:
 # Weekly, Monthly, Total so far
 #
 class Hours
   def initialize
     @hours = []
+    @cycling = 0
+    @entertainment = 0
+    @programming = 0
+    @reading = 0
+    @yoga = 0
   end
   def add_hours hours
     @hours.push(hours)
+  end
+  def sum_hours
+    @hours.each do |row|
+      @cycling += row[0].to_f
+      @entertainment += row[1].to_f
+      @programming += row[2].to_f
+      @reading += row[3].to_f
+      @yoga += row[4].to_f
+    end
+  end
+  def print_hours
+    puts "Cycling: \t #@cycling hours"
+    puts "Entertainment: \t #@entertainment hours"
+    puts "Programming: \t #@programming hours"
+    puts "Reading: \t #@reading hours"
+    puts "Yoga: \t #@yoga hours"
   end
 end
 
@@ -19,3 +40,5 @@ my_hours = Hours.new
 File.open(ARGV[0]).each_line do |line|
   my_hours.add_hours line.split(" ")
 end
+my_hours.sum_hours
+my_hours.print_hours
